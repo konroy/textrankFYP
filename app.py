71fae7a36ef1769915028db1f1ac4293baa6b39d
Ahmad Asyraf <42167998+konroy@users.noreply.github.com>
2021-01-31 14:11:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 import streamlit as st
-#from summa import summarizer
 import numpy as np
 from newspaper import Article
 from nltk.tokenize.punkt import PunktSentenceTokenizer
-#import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer, TfidfVectorizer
 import networkx as nx
-#import pydot as pd
-#from pyvis.network import Network
 import pandas as pd
 
 def clean_text(text):
@@ -23,11 +19,6 @@ def parse(url):
     	text = "Oops! No content."
     	return text
 
-#opts, args = getopt.getopt(argv, '')
-
-    #if len(argv) == 0:
-        #print('No URL found', file=sys.stderr)
-
 def inputURL():
 	url = st.text_input("Input URL", "",)
 	return url
@@ -35,20 +26,12 @@ def inputURL():
 def tokenize(text):
 	doc_tokenizer = PunktSentenceTokenizer()
 	sentences_list = doc_tokenizer.tokenize(text)
-	#st.write(sentences_list)
 	return sentences_list
 
 def tdMatrix(sentence):
 	cv = CountVectorizer()
 	cv_matrix = cv.fit_transform(sentence)
-	#st.write(cv_matrix)
 	normal_matrix = TfidfTransformer().fit_transform(cv_matrix)
-	#tfidf = TfidfVectorizer()
-	#matrix = tfidf.fit_transform(sentence)
-	#df_temp = pd.DataFrame(matrix.toarray(), columns = tfidf.vocabulary_)
-	#df_T = df_temp.T
-	#df_T.to_csv('tfidf.csv')
-	#st.dataframe(df_temp.T)
 	res_graph = normal_matrix * normal_matrix.T
 	return res_graph
 
@@ -82,13 +65,9 @@ def textrank(nx_graph,sentences_list):
 		for i in range(0, len(sentence_array)):
 			temp_array.append((float(sentence_array[i][0]) - rank_min) / (rank_max - rank_min))
 			df_temp.at[i,'Score'] = temp_array[i]
-
-	#df_temp = pd.DataFrame(sentence_array, columns = ['Score', 'Sentence'])
-	#for i in range(0, len(sentence_array)):
-		#df_temp.at[i,'Score'] = temp_array[i]
+	
 	st.header("Scoring")
 	st.table(df_temp)
-	#df_temp.to_csv('textrank.csv')
 
 	threshold = (sum(temp_array) / len(temp_array)) + 0.2
 	st.write("The threshold of the summarization is ",threshold)
@@ -101,11 +80,6 @@ def textrank(nx_graph,sentences_list):
 		sentences_list.append(sentence_array[0][1])
 	
 	return sentences_list
-	#summary = " ".join(str(x) for x in sentences_list)
-	#return summary
-	
-	#summarized_text = summarizer.summarize(text, ratio=ratio, language="english", split=True, scores=True)
-	#return summarized_text
 
 def main():
 	st.title("Summarizer")
@@ -115,23 +89,13 @@ def main():
 	text = st.text_area("Text", parse(url), height=500)
 
 	st.title("Results")
-	
-	
-	#text = st.text_area("Text", "", height=500)
-
-	#ratio = st.slider("Summarization fraction", min_value=0.0, max_value=1.0, value=0.2, step=0.01)
 
 	sentences_list = tokenize(text)
 
 	res_graph = tdMatrix(sentences_list)
-	#st.write(type(res_graph))
 	nx_graph = nxGraph(res_graph)
 
-	#nx_graph = nxGraph(graph)
-
-	
 	summary = textrank(nx_graph,sentences_list)
-	#st.write(summary)
 
 	st.header("Summary")
 	for lines in summary:
@@ -139,7 +103,5 @@ def main():
 
 	st.header("Graph")
 	showGraph(nx_graph)
-	#for sentence, score in summary:
-	#	st.write(sentence)
 
 main()
